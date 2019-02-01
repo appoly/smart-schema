2bc89bf8240f3419c79ae4110d928dd987c9f288
John Wedgbury <johnw@wedgbury.net>
2019-02-01 13:38:15 +0000

    Create radio.blade.php

---
+++ src/views/impl/radio.blade.php
@@ -0,0 +1,25 @@
+<div class="form-group">
+    <label for="{{ $field['name'] }}">{{ $field['label'] }}</label>
+    <?php
+    $old_value = old($field['name'],
+        isset($config['initial'][$field['name']]) ? $config['initial'][$field['name']] : '');
+    ?>
+    @foreach($config['select_options'][$field['name']] ?? $config['select_options'] as $key => $label)
+        <div class="custom-control custom-radio">
+            <input type="radio" id="{{ $field['name'] }}-{{ $key }}" name="{{ $field['name'] }}" value="{{ $key }}" class="custom-control-input" {{ ($old_value == $key ? 'selected' : ' ') }}>
+            <label class="custom-control-label" for="{{ $field['name'] }}-{{ $key }}">{{ $label }}</label>
+        </div>
+    @endforeach
+
+    @if(isset($field['help']))
+        <small id="{{ $field['name'] }}Help" class="form-text text-muted">{{ $field['help'] }}</small>
+    @endif
+
+    @if($errors->has($field['name']))
+        @foreach($errors->get($field['name']) as $message)
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @endforeach
+    @endif
+</div>
