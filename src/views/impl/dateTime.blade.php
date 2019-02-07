056231a718cb17449c3c8ab26482e892fc7f52c6
John Wedgbury <johnw@wedgbury.net>
2019-02-07 10:48:05 +0000

    Create dateTime.blade.php

---
+++ src/views/impl/dateTime.blade.php
@@ -0,0 +1,29 @@
+<div class="form-group">
+  <label for="{{ $field['name'] }}">{{ $field['label'] }}</label>
+
+  <?php
+  $old_val = old($field['name'], isset($config['initial'][$field['name']]) ? $config['initial'][$field['name']] : '');
+
+   if($old_val instanceof \Carbon\Carbon || $old_val instanceof Illuminate\Support\Carbon)
+     $old_val = $old_val->format('Y-m-d H:i:s');
+  ?>
+
+  <div class="input-group datepicker" id="{{ $field['name'] }}" data-target-input="nearest">
+    <input type="text" name="{{ $field['name'] }}" value="{{ $old_val }}" class="form-control datetimepicker-input {{ ($errors->has($field['name'])) ? ' is-invalid' : '' }}"
+      data-target="#{{ $field['name'] }}" />
+    <div class="input-group-append" data-target="#{{ $field['name'] }}" data-toggle="datetimepicker">
+      <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+    </div>
+  </div>
+
+  @if(isset($field['help']))
+  <small id="{{ $field['name'] }}Help" class="form-text text-muted">{{ $field['help'] }}</small>
+  @endif
+  @if($errors->has($field['name']))
+  @foreach($errors->get($field['name']) as $message)
+  <div class="invalid-feedback">
+    {{ $message }}
+  </div>
+  @endforeach
+  @endif
+</div>
