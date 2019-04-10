2e9fd352cb37a3247d1f871e41be08d3680b737c
Joe Miller <joe@appoly.co.uk>
2019-04-10 14:08:32 +0100

    Adding number input type

---
+++ src/views/impl/number.blade.php
@@ -0,0 +1,37 @@
+<?php
+$old_value = old($field['name'], isset($config['initial'][$field['name']]) ? $config['initial'][$field['name']] : '');
+?>
+<div class="form-group">
+    <label for="{{ $field['name'] }}">{{ $field['label'] }}</label>
+    <div class="input-group">
+        @if(isset($field['prepend']))
+            <div class="input-group-prepend">
+                <span class="input-group-text">{{ $field['prepend'] }}</span>
+            </div>
+        @endif
+        <input name="{{ $field['name'] }}" type="number"
+               class="form-control {{ ($errors->has($field['name'])) ? ' is-invalid' : '' }}" id="{{ $field['name'] }}"
+               step="{{ isset($field['step']) ? $field['step'] : 'any' }}"
+               @if(isset($field['placeholder']))
+               placeholder="{{ $field['placeholder'] }}"
+               @endif
+               aria-describedby="{{ $field['name'] }}Help"
+               value="{{ $old_value }}">
+        @if(isset($field['append']))
+            <div class="input-group-append">
+                <span class="input-group-text">{{ $field['append'] }}</span>
+            </div>
+        @endif
+    </div>
+    @if(isset($field['help']))
+        <small id="{{ $field['name'] }}Help" class="form-text text-muted">{{ $field['help'] }}</small>
+    @endif
+
+    @if($errors->has($field['name']))
+        @foreach($errors->get($field['name']) as $message)
+            <div class="invalid-feedback d-block">
+                {{ $message }}
+            </div>
+        @endforeach
+    @endif
+</div>
