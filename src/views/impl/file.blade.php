e38bc5f495d6412b01967c18f153a243a81b54b9
Calum Chamberlain <34173688+CalumChamberlain@users.noreply.github.com>
2019-04-26 12:31:30 +0100

    Change file form field layout

--- src/views/impl/file.blade.php
+++ src/views/impl/file.blade.php
@@ -1,6 +1,15 @@
 <div class="form-group">
     @if(isset($field['label']))
-        <label for="{{ $field['name'] }}">{{ $field['label'] }}</label>
+    <label for="{{ $field['name'] }}">{{ $field['label'] }}</label>
     @endif
-    <input type="file" accept="{{ isset($field['accept']) ? $field['accept'] : '' }}" name="{{ $field['name'] }}">
+    <input type="file" accept="{{ isset($field['accept']) ? $field['accept'] : '' }}" name="{{ $field['name'] }}"
+        class='w-100 {{ $errors->has('pdf[map_under_18s]') ? "is-invalid" : "" }}'>
+
+    @if($errors->has($field['name']))
+    @foreach($errors->get($field['name']) as $message)
+    <div class="invalid-feedback">
+        {{ $message }}
+    </div>
+    @endforeach
+    @endif
 </div>
